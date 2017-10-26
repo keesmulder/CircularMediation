7197baffd532867ca783c4cf8a0773d3eff02c7a
Kees Mulder <keestimmulder@gmail.com>
2017-10-26 11:32:11 +0200

    Added CI and p-values

--- CircularRegressionMediationBootstrap.R
+++ CircularRegressionMediationBootstrap.R
@@ -1,7 +1,7 @@
 library(boot)
 library(circular)
 
-# Function to compute
+# Function to compute a single circular mediation effect.
 # data is a data.frame with named columns.
 # outcome is a string giving the name of the outcome
 # predictor is a string giving the name of the predictor
@@ -34,7 +34,34 @@ computeCircMediation <- function(data, inds, outcome, predictor, mediators) {
     indirect_effect = a_tilde - a)
 }
 
-# Convenience function to only obtain one on the results provided.
+# Function to compute a single circular mediation effect.
+# data is a data.frame with named columns.
+# outcome is a string giving the name of the outcome
+# predictor is a string giving the name of the predictor
+# mediators is a string vector giving the name of the mediator
+# R is the number of bootstrap iterations
+# probs is numeric vector giving the proportions at which quantiles should be given
+circMediationBootstrap <- function(data, outcome, predictor, mediators, R = 1000, probs = c(.025, .975), ...) {
+
+  # Compute the bootstrap using the boot package.
+  circmedboot <- boot(data, computeCircMediation, R = R, outcome = outcome, predictor = predictor, mediators = mediators, ...)
+
+  # Obtain the p-values for t0 > 0, which will be taken as 1 - p for t0 < 0.
+  ps <- apply(circmedboot$t, 2L, function(x) mean(x <= 0))
+
+  # Obtain result
+  cbind(original     = circmedboot$t0,
+        bias         = apply(circmedboot$t, 2L, mean, na.rm = TRUE) - circmedboot$t0,
+        "std. error" = sqrt(apply(circmedboot$t, 2L, function(t.st) var(t.st[!is.na(t.st)]))),
+        t(apply(circmedboot$t, 2L, function(x) quantile(x, probs = probs))),
+        "p-value"    = ifelse(circmedboot$t0 > 0, ps, 1 - ps))
+}
 
 
-boot(data, computeCircMediation, R = 10)
+# circmedboot <- circMediationBootstrap(data, outcome = "ips", predictor = "culture", mediators = c("agaze", "cgaze"), R = 1000)
+# agazecmb <- circMediationBootstrap(data, outcome = "ips", predictor = "culture", mediators = "agaze", R = 1000)
+# cgazecmb <- circMediationBootstrap(data, outcome = "ips", predictor = "culture", mediators = "cgaze", R = 1000)
+#
+# circmedboot
+# agazecmb
+# cgazecmb
