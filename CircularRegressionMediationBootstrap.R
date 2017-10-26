9756ae01a68ae2fa93b5f7b4770179e3755fe9d6
Kees Mulder <keestimmulder@gmail.com>
2017-10-26 15:53:06 +0200

    Explicitly state which p-value is the one-sided and which is the two-sided

--- CircularRegressionMediationBootstrap.R
+++ CircularRegressionMediationBootstrap.R
@@ -34,6 +34,9 @@ computeCircMediation <- function(data, inds, outcome, predictor, mediators) {
     indirect_effect = a_tilde - a)
 }
 
+
+print.circMedBoot <- function(object, ...) print(object$tab, ...)
+
 # Function to compute a single circular mediation effect.
 # data is a data.frame with named columns.
 # outcome is a string giving the name of the outcome
@@ -44,16 +47,24 @@ computeCircMediation <- function(data, inds, outcome, predictor, mediators) {
 circMediationBootstrap <- function(data, outcome, predictor, mediators, R = 1000, probs = c(.025, .975), ...) {
 
   # Compute the bootstrap using the boot package.
-  circmedboot <- boot(data, computeCircMediation, R = R, outcome = outcome, predictor = predictor, mediators = mediators, ...)
+  # suppressWarnings({
+    circmedboot <- boot(data, computeCircMediation, R = R, outcome = outcome, predictor = predictor, mediators = mediators, ...)
+  # })
 
   # Obtain the p-values for t0 > 0, which will be taken as 1 - p for t0 < 0.
   ps <- apply(circmedboot$t, 2L, function(x) mean(x <= 0))
 
   # Obtain result
-  cbind(original     = circmedboot$t0,
-        bias         = apply(circmedboot$t, 2L, mean, na.rm = TRUE) - circmedboot$t0,
-        "std. error" = sqrt(apply(circmedboot$t, 2L, function(t.st) var(t.st[!is.na(t.st)]))),
-        t(apply(circmedboot$t, 2L, function(x) quantile(x, probs = probs))),
-        "p-value"    = ifelse(circmedboot$t0 > 0, ps, 1 - ps))
+  res <- list(tab = cbind(original     = circmedboot$t0,
+                         bias         = apply(circmedboot$t, 2L, mean, na.rm = TRUE) - circmedboot$t0,
+                         "std. error" = sqrt(apply(circmedboot$t, 2L, function(t.st) var(t.st[!is.na(t.st)]))),
+                         t(apply(circmedboot$t, 2L, function(x) quantile(x, probs = probs))),
+                         "one-sided p-value"    = ifelse(circmedboot$t0 > 0, ps, 1 - ps),
+                         "two-sided p-value"    = ifelse(circmedboot$t0 > 0, ps*2, (1 - ps) * 2 )),
+              bootsam = circmedboot$t,
+              bootobj = circmedboot)
+
+  class(res) <- c("circMedBoot", class(res))
+  res
 }
 
